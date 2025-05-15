9fc3ab856dacaea9375012200bef3700998d205e
Ivan23L <ivanlzrz23@gmail.com>
2025-05-15 18:53:28 +0200

    directorios

--- php/util/config.php
+++ php/util/config.php
@@ -6,16 +6,15 @@
     // $contrasena = "";
     // $BBDD = "weconnectdb";
 
-   /* $servidor = "localhost";
+    $servidor = "localhost";
     $usuario = "u593365251_root";
     $contrasena = "Patatata12345!";
-    $BBDD = "u593365251_weConnect";*/
+    $BBDD = "u593365251_weConnect";
 
-   //Conexion Ernesto
-    $servidor = "localhost";
-    $usuario = "root";
-    $contrasena = "";
-    $BBDD = "We_connect";
+    // $servidor = "localhost";
+    // $usuario = "root";
+    // $contrasena = "";
+    // $BBDD = "We_connect";
 
     //Mysqli ó PDO (nosotros vamos a usar Mysqli)
     //Intenta crear una conexion con la base de datos con los siguientes parámetros y en ese orden
